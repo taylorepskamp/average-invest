8a4487c15b230bfe5fcec275f41ffab8a4a55df3
Taylor Epskamp <taylorepskamp@gmail.com>
2020-04-30 12:55:37 +0200

    Updates

---
+++ precache-manifest.59035f1b7050cc853c0624aacae077e0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb930b089152fc3d46837d293bcf9e8",
+    "url": "/reactApp2/index.html"
+  },
+  {
+    "revision": "a69c29c82af5bed49f2c",
+    "url": "/reactApp2/static/css/main.27ccf5a8.chunk.css"
+  },
+  {
+    "revision": "258e10ec6e103843c58b",
+    "url": "/reactApp2/static/js/2.0227767a.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/reactApp2/static/js/2.0227767a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69c29c82af5bed49f2c",
+    "url": "/reactApp2/static/js/main.3fcac039.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/reactApp2/static/js/main.3fcac039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be84f4dd4932d322b58",
+    "url": "/reactApp2/static/js/runtime-main.5333aef0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactApp2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactApp2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactApp2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactApp2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactApp2/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/reactApp2/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/reactApp2/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/reactApp2/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/reactApp2/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/reactApp2/static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4634f8d070a11fa951aa584b3f8958a2.js"
+  "/reactApp2/precache-manifest.59035f1b7050cc853c0624aacae077e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactApp2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
