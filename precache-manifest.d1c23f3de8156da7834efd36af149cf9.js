a1acfcae16121abc27d33d2b401144d9da3b06d0
Taylor Epskamp <taylorepskamp@gmail.com>
2020-04-30 12:50:32 +0200

    Updates

---
+++ precache-manifest.d1c23f3de8156da7834efd36af149cf9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7094ddc6128d17a232991a8f3438f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e10be03b643d54cd453",
+    "url": "/static/css/main.c93973b8.chunk.css"
+  },
+  {
+    "revision": "d03e582c3299e60b801a",
+    "url": "/static/js/2.760ff4bd.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/static/js/2.760ff4bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e10be03b643d54cd453",
+    "url": "/static/js/main.b1805837.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/static/js/main.b1805837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4c7ed4d2b080902791",
+    "url": "/static/js/runtime-main.d64b10c7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
