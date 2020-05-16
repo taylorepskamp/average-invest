84519fb9f48ed5dc5d047aeaf6f7e2997414e5de
Taylor Epskamp <taylorepskamp@gmail.com>
2020-05-16 16:08:08 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactApp2/precache-manifest.f03db621c51c3fa8a59e3cbf7b0c2763.js"
+  "/reactApp2/precache-manifest.9d5cb8bd3448e7952b9ff73baebd3ba4.js"
 );
 
 self.addEventListener('message', (event) => {
